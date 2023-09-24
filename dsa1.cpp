9f99aac6f2c914a376e49614dc8aa13fb981c546
Avitoshsandve <129187663+Avitoshsandve@users.noreply.github.com>
2023-09-24 23:05:19 +0530

    Add files via upload

---
+++ dsa1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Welcome to my DSA journey." << endl;
+    cout << "1.Goal is to complete it within 50 days." << endl;
+    cout << "2.so lets do it regularly,consistently." << endl;
+    return 0;
+}
