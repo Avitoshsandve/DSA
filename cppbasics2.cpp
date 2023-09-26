42af510272141e94b2f6c6b2a591845f2fc029b2
Avitoshsandve <129187663+Avitoshsandve@users.noreply.github.com>
2023-09-27 02:05:50 +0530

    Add files via upload

---
+++ cppbasics2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i = 5;
+    if (i < 1)
+    {
+        cout << "avitosh";
+    }
+    else if (i > 1)
+    {
+        cout << "Sandve";
+    }
+    else
+    {
+        cout << "not possible";
+    }
+    return 0;
+}
