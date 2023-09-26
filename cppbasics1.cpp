f481dfbfca4abdb94c557fb5706111c3090b25e7
Avitoshsandve <129187663+Avitoshsandve@users.noreply.github.com>
2023-09-27 01:54:39 +0530

    Add files via upload

---
+++ cppbasics1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i=5;
+    if(i>1){
+        cout<<"DSA is easy";
+    }
+    else{
+        cout<<"Actually it's not";
+    }
+    return 0;
+}
