47ec96bbe8a6237e2a59f5c3af4d50d4baefe136
Avitoshsandve <129187663+Avitoshsandve@users.noreply.github.com>
2023-10-04 22:51:54 +0530

    Add files via upload

---
+++ cpp string basics1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    //defining string in string style
+    string s="Avitosh";
+    cout<<s;
+    cout<<" will complete DSA in 50 days.";
+
+    
+    return 0;
+}
