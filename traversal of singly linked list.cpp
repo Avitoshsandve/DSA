e22bb5ab5b83ac925fc28be11c6793312d5cc526
Avitoshsandve <129187663+Avitoshsandve@users.noreply.github.com>
2024-03-02 23:52:22 +0530

    Add files via upload

---
+++ traversal of singly linked list.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+class node
+{
+    public:
+    int data;
+    node *next;
+    node(int data)
+    {
+        this->data=data;
+        next=NULL;
+    }
+};
+void traversallist(node* &head)
+{
+    node *temp=head;
+    while(temp!=NULL)
+    {
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+    cout<<endl;
+}
+
+int main()
+{
+    node *head=new node(1);
+    head->next=new node(2);
+    head->next->next=new node(3);
+    head->next->next->next=new node(4);
+    head->next->next->next->next=new node(5);
+    traversallist(head);
+    return 0;
+}
