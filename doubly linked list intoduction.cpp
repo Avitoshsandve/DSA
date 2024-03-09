514da2d62a71b57e2ba453334397e470be45b7a8
Avitoshsandve <129187663+Avitoshsandve@users.noreply.github.com>
2024-03-10 00:52:22 +0530

    Add files via upload

---
+++ doubly linked list intoduction.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+
+struct node{
+    int data;
+    struct node *prev;
+    struct node *next;
+    
+};
+
+int main() 
+{
+    node* head_node = new node();
+    head_node->data = 1;
+    head_node->prev = NULL;
+    node* second_node = new node();
+    second_node->data = 2;
+    second_node->prev = head_node;
+    second_node->next = NULL;
+    head_node->next = second_node;
+    
+ 
+    while(head_node != NULL)
+    {
+        cout<<head_node->data<<" ";
+        head_node = head_node->next;
+    }
+    
+    return 0;
+}
